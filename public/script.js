8748ca639cab27a6cd39225705562e48f19d8e2e
Benoît Bernier <43164960+benoit-bernier@users.noreply.github.com>
2022-04-01 15:21:18 +0200

    update package.json, réduction des informations demandées dans la requête graphql

--- public/script.js
+++ public/script.js
@@ -1,7 +1,23 @@
 var bearerApiDS = sessionStorage.getItem("DS");
 
 document.getElementById("archive-btn").addEventListener("click", () => {
-  let url =
-    `http://localhost:3000/archive/${document.getElementById("search-787-input").value}?bearer=${bearerApiDS}`;
-  window.open(url);
+  download_archive();
 });
+document
+  .getElementById("search-787-input")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      download_archive();
+    }
+  });
+  
+function download_archive() {
+  if (bearerApiDS) {
+    let url = `http://localhost:3000/archive/${
+      document.getElementById("search-787-input").value
+    }?bearer=${bearerApiDS}`;
+    window.open(url);
+  } else {
+    document.getElementById("alert_token").style.display = "block";
+  }
+}
--- server.js
+++ server.js
@@ -1,317 +1,66 @@
-//import { request, gql } from "graphql-request";
 const graphql = require("graphql-request");
 const express = require("express");
-const zip = require("express-zip");
 const path = require("path");
-const bodyParser = require("body-parser");
 const fs = require("fs");
 const archiver = require("archiver");
-const rimraf = require("rimraf");
 const https = require("https");
 
 const app = express();
 const port = 3000;
 
-const query = graphql.gql`
-  query getDemarche(
-    $demarcheNumber: Int!
-    $state: DossierState
-    $order: Order
-    $after: String
-  ) {
-    demarche(number: $demarcheNumber) {
-      id
-      number
-      title
-      publishedRevision {
-        ...RevisionFragment
-      }
-      groupeInstructeurs {
-        id
-        number
-        label
-        instructeurs {
-          id
-          email
-        }
-      }
-      dossiers(state: $state, order: $order, after: $after) {
-        nodes {
-          ...DossierFragment
-        }
-      }
-    }
-  }
-
-  fragment DossierFragment on Dossier {
+const query = graphql.gql`query getDemarche(
+  $demarcheNumber: Int!
+) {
+  demarche(number: $demarcheNumber) {
     id
     number
-    archived
-    state
-    dateDerniereModification
-    datePassageEnConstruction
-    datePassageEnInstruction
-    dateTraitement
-    motivation
-    motivationAttachment {
-      ...FileFragment
-    }
-    attestation {
-      ...FileFragment
-    }
-    pdf {
-      url
-    }
-    instructeurs {
-      email
-    }
-    groupeInstructeur {
-      id
-      number
-      label
-    }
-    revision {
-      ...RevisionFragment
-    }
-    champs {
-      ...ChampFragment
-      ...RootChampFragment
-    }
-    annotations {
-      ...ChampFragment
-      ...RootChampFragment
-    }
-    avis {
-      ...AvisFragment
-    }
-    messages {
-      ...MessageFragment
-    }
-    demandeur {
-      ... on PersonnePhysique {
-        civilite
-        nom
-        prenom
-        dateDeNaissance
-      }
-      ...PersonneMoraleFragment
-    }
-  }
-
-  fragment RevisionFragment on Revision {
-    id
-    champDescriptors {
-      ...ChampDescriptorFragment
-      champDescriptors {
-        ...ChampDescriptorFragment
-      }
-    }
-    annotationDescriptors {
-      ...ChampDescriptorFragment
-      champDescriptors {
-        ...ChampDescriptorFragment
+    dossiers {
+      nodes {
+        ...DossierFragment
       }
     }
   }
+}
 
-  fragment ChampDescriptorFragment on ChampDescriptor {
-    id
-    type
-    label
-    description
-    required
-    options
-  }
-
-  fragment AvisFragment on Avis {
-    id
-    question
-    reponse
-    dateQuestion
-    dateReponse
-    claimant {
-      email
-    }
-    expert {
-      email
-    }
-    attachment {
-      ...FileFragment
-    }
-  }
-
-  fragment MessageFragment on Message {
-    id
-    email
-    body
-    createdAt
-    attachment {
-      ...FileFragment
-    }
-  }
-
-  fragment GeoAreaFragment on GeoArea {
-    id
-    source
-    description
-    geometry {
-      type
-      coordinates
-    }
-    ... on ParcelleCadastrale {
-      commune
-      numero
-      section
-      prefixe
-      surface
-    }
-  }
-
-  fragment RootChampFragment on Champ {
-    ... on RepetitionChamp {
-      champs {
-        ...ChampFragment
-      }
-    }
-    ... on SiretChamp {
-      etablissement {
-        ...PersonneMoraleFragment
-      }
-    }
-    ... on CarteChamp {
-      geoAreas {
-        ...GeoAreaFragment
-      }
-    }
-    ... on DossierLinkChamp {
-      dossier {
-        id
-        state
-        usager {
-          email
-        }
-      }
-    }
-  }
-
-  fragment ChampFragment on Champ {
-    id
-    label
-    stringValue
-    ... on DateChamp {
-      date
-    }
-    ... on DatetimeChamp {
-      datetime
-    }
-    ... on CheckboxChamp {
-      checked: value
-    }
-    ... on DecimalNumberChamp {
-      decimalNumber: value
-    }
-    ... on IntegerNumberChamp {
-      integerNumber: value
-    }
-    ... on CiviliteChamp {
-      civilite: value
-    }
-    ... on LinkedDropDownListChamp {
-      primaryValue
-      secondaryValue
-    }
-    ... on MultipleDropDownListChamp {
-      values
-    }
-    ... on PieceJustificativeChamp {
-      file {
-        ...FileFragment
-      }
-    }
-    ... on AddressChamp {
-      address {
-        ...AddressFragment
-      }
-    }
-    ... on CommuneChamp {
-      commune {
-        name
-        code
-      }
-      departement {
-        name
-        code
-      }
-    }
-  }
-
-  fragment PersonneMoraleFragment on PersonneMorale {
-    siret
-    siegeSocial
-    naf
-    libelleNaf
-    address {
-      ...AddressFragment
-    }
-    entreprise {
-      siren
-      capitalSocial
-      numeroTvaIntracommunautaire
-      formeJuridique
-      formeJuridiqueCode
-      nomCommercial
-      raisonSociale
-      siretSiegeSocial
-      codeEffectifEntreprise
-      dateCreation
-      nom
-      prenom
-      attestationFiscaleAttachment {
-        ...FileFragment
-      }
-      attestationSocialeAttachment {
-        ...FileFragment
-      }
-    }
-    association {
-      rna
-      titre
-      objet
-      dateCreation
-      dateDeclaration
-      datePublication
-    }
-  }
-
-  fragment FileFragment on File {
-    filename
-    contentType
-    checksum
-    byteSizeBigInt
+fragment DossierFragment on Dossier {
+  number
+  pdf {
     url
   }
-
-  fragment AddressFragment on Address {
-    label
-    type
-    streetAddress
-    streetNumber
-    streetName
-    postalCode
-    cityName
-    cityCode
-    departmentName
-    departmentCode
-    regionName
-    regionCode
+  champs {
+    ...ChampFragment
+    ...RootChampFragment
   }
-`;
+}
 
-//const numero_demarche = 57091;
+fragment RootChampFragment on Champ {
+  ... on RepetitionChamp {
+    champs {
+      ...ChampFragment
+    }
+  }
+}
+
+fragment ChampFragment on Champ {
+  id
+  label
+  stringValue
+  ... on PieceJustificativeChamp {
+    file {
+      ...FileFragment
+    }
+  }
+}
+
+fragment FileFragment on File {
+  filename
+  contentType
+  checksum
+  byteSizeBigInt
+  url
+}`;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -321,109 +70,153 @@ app.get("/token.html", function (req, res) {
   res.sendFile(path.join(__dirname, "token.html"));
 });
 
-app.get("/test", (req, res) => {
-  res
-    .attachment(
-      "C:\\Users\\bbernier\\Desktop\\stage\\archiveur_ds\\temp\\57091\\57091.zip"
-    )
-    .send();
-});
-
 app.get("/archive/:numero_demarche", async (req, res) => {
   var numero_demarche = req.params.numero_demarche.toString();
   var bearerApiDS = req.query.bearer;
-  mkdir("temp/" + numero_demarche);
-  graphql
-    .request({
+  mkdir(`temp/${numero_demarche}_temp/${numero_demarche}`);
+  try {
+    const data = await graphql.request({
       url: "https://www.demarches-simplifiees.fr/api/v2/graphql",
       document: query,
       variables: { demarcheNumber: parseInt(numero_demarche) },
       requestHeaders: {
         authorization: `Bearer ${bearerApiDS}`,
       },
-    })
-    .then((data) => {
-      console.log(data);
-      fs.writeFileSync(
-        path.join(
-          __dirname,
-          "temp",
-          numero_demarche,
-          `${data.demarche.number}.json`
-        ),
-        JSON.stringify(data)
+    });
+    console.log(data);
+    fs.writeFileSync(
+      path.join(
+        __dirname,
+        "temp",
+        `${numero_demarche}_temp`,
+        numero_demarche,
+        `${data.demarche.number}.json`
+      ),
+      JSON.stringify(data)
+    );
+    data.demarche.dossiers.nodes.forEach(async (dossier) => {
+      // Téléchargement du résumé pdf de chaque dossier
+      let path_for_download = path.join(
+        __dirname,
+        "temp",
+        `${numero_demarche}_temp`,
+        numero_demarche,
+        `${dossier.number.toString()}` //-${dossier.demandeur.nom}-${dossier.demandeur.prenom}`
       );
-      data.demarche.dossiers.nodes.forEach(async (dossier) => {
-        // Téléchargement du résumé pdf de chaque dossier
-        let path_for_download = path.join(
-          __dirname,
-          "temp",
-          numero_demarche,
-          `${dossier.number.toString()}-${dossier.demandeur.nom}-${
-            dossier.demandeur.prenom
-          }`
-        );
-        mkdir(path_for_download);
-        await download_file(path_for_download, "demarche.pdf", dossier.pdf.url);
-        // Parcours des champs, recherche des champs pièce justificative
-        dossier.champs.forEach((element) => {
-          if (element.file) {
-            //Si le champ comporte une indication "file", on le télécharge
-            telechargement_fichier(dossier, element, numero_demarche);
-          } else if (element.champs) {
-            // Même chose pour les champs répétables
-            element.champs.forEach((champ_repetable) => {
-              if (champ_repetable.file) {
-                telechargement_fichier(
-                  dossier,
-                  champ_repetable,
-                  numero_demarche
-                );
-              }
-            });
-          }
-        });
-        //Compression du dossier au format .zip
-
-        var demarche_dossier = path.join(__dirname, "temp", numero_demarche);
-        var demarche_dossier_cible = path.join(
-          __dirname,
-          "temp",
-          numero_demarche,
-          `${numero_demarche}.zip`
-        );
-        zipDirectory(demarche_dossier, demarche_dossier_cible).then(
-          async (e) => {
-            //une fois le dossier compressé on l'envoie
-            res.attachment(demarche_dossier_cible).send();
-
-            //une fois le zip envoyé, on supprime tout ce qu'on a créé.
-            try {
-              fs.rmdir(demarche_dossier, { recursive: true }, (error) => {
-                if (error) {
-                  console.error(error);
-                } else {
-                  console.log(`Dossier ${demarche_dossier} supprimé`);
-                }
-              });
-              console.log(`${demarche_dossier} supprimé`);
-            } catch (err) {
-              console.error(`Error while deleting ${dir}.`);
+      mkdir(path_for_download);
+      await download_file(path_for_download, "demarche.pdf", dossier.pdf.url);
+      // Parcours des champs, recherche des champs pièce justificative
+      await dossier.champs.forEach(async (element) => {
+        if (element.file) {
+          //Si le champ comporte une indication "file", on le télécharge
+          await telechargement_fichier(dossier, element, numero_demarche);
+        } else if (element.champs) {
+          // Même chose pour les champs répétables
+          await element.champs.forEach(async (champ_repetable) => {
+            if (champ_repetable.file) {
+              await telechargement_fichier(
+                dossier,
+                champ_repetable,
+                numero_demarche
+              );
             }
-          }
-        );
+          });
+        }
       });
     });
+  } catch (error) {
+    switch (
+      JSON.parse(JSON.stringify(error, undefined, 2)).response.errors[0]
+        .extensions.code
+    ) {
+      case "unauthorized":
+        try {
+          fs.writeFileSync(
+            `${path_for_download}.txt`,
+            "Action non autorisée. Votre token ne semble pas pouvoir accéder à cette démarche."
+          );
+        } catch (error) {}
+        break;
+      case "not_found ":
+        try {
+          fs.writeFileSync(
+            `${path_for_download}.txt`,
+            "La démarche cherchée n'a pas été trouvée."
+          );
+        } catch (error) {}
+        break;
+      default:
+        try {
+          fs.writeFileSync(
+            `${path_for_download}.txt`,
+            "Une erreur inconnue est survenue."
+          );
+        } catch (error) {}
+        break;
+    }
+    console.log();
+  }
+
+  //Compression du dossier au format .zip
+
+  var demarche_dossier = path.join(
+    __dirname,
+    "temp",
+    `${numero_demarche}_temp`,
+    numero_demarche
+  );
+  var demarche_dossier_cible = path.join(
+    __dirname,
+    "temp",
+    `${numero_demarche}_temp`,
+    `${numero_demarche}.zip`
+  );
+
+  await zipDirectory(demarche_dossier, demarche_dossier_cible);
+  //une fois le dossier compressé on l'envoie
+  console.log("Fichier à envoyer :", demarche_dossier_cible);
+  res.download(demarche_dossier_cible, (err) => {
+    console.log("download callback");
+    //deleteFolderRecursive(path.join(__dirname, "temp", `${numero_demarche}_temp`))
+    let dossier_a_supprimer = path.join(
+      __dirname,
+      "temp",
+      `${numero_demarche}_temp`
+    );
+    fs.rmdir(dossier_a_supprimer, { recursive: true, force: true, maxRetries:3}, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("Suppression effectuées : ", dossier_a_supprimer);
+      }
+    });
+  });
 });
+
+function deleteFolderRecursive(directoryPath) {
+  if (fs.existsSync(directoryPath)) {
+    fs.readdirSync(directoryPath).forEach((file, index) => {
+      console.log(file);
+      const curPath = path.join(directoryPath, file);
+      if (fs.lstatSync(curPath).isDirectory()) {
+        // recurse
+        deleteFolderRecursive(curPath);
+      } else {
+        // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(directoryPath);
+  }
+}
 
 async function telechargement_fichier(dossier, element, numero_demarche) {
   let path_for_download = path.join(
     __dirname,
     "temp",
+    `${numero_demarche}_temp`,
     numero_demarche,
-    `${dossier.number.toString()}-${dossier.demandeur.nom}-${
-      dossier.demandeur.prenom
-    }`,
+    `${dossier.number.toString()}`, //-${dossier.demandeur.nom}-${dossier.demandeur.prenom}`,
     "pieces_justificatives"
   );
   mkdir(path_for_download);
@@ -436,18 +229,21 @@ async function telechargement_fichier(dossier, element, numero_demarche) {
 
 function mkdir(dir) {
   try {
-    fs.mkdirSync(dir);
+    fs.mkdirSync(dir, { recursive: true });
   } catch (error) {
     //console.error(error);
   }
 }
 
-function download_file(folder, file_name, url) {
-  const file = fs.createWriteStream(`${folder}/${file_name}`);
-  https.get(url, (response) => {
-    response.pipe(file);
-    file.on("finish", () => {
-      file.close();
+async function download_file(folder, file_name, url) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(`${folder}/${file_name}`);
+    https.get(url, (response) => {
+      response.pipe(file);
+      file.on("finish", () => {
+        file.close();
+        resolve();
+      });
     });
   });
 }
@@ -457,7 +253,7 @@ function download_file(folder, file_name, url) {
  * @param {String} outPath: /path/to/created.zip
  * @returns {Promise}
  */
-function zipDirectory(sourceDir, outPath) {
+async function zipDirectory(sourceDir, outPath) {
   const archive = archiver("zip", { zlib: { level: 9 } });
   const stream = fs.createWriteStream(outPath);
 
